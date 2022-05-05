2d6435d51e8059f9db4872b7c94a415848f4384a
ssarinha <102757853+ssarinha@users.noreply.github.com>
2022-05-05 15:42:25 -0300

    Add files via upload

---
+++ funçoes.js
@@ -0,0 +1,23 @@
+function name (b){
+console.log(b)
+    return
+}
+
+name ('Sara Sousa')
+
+//3
+function soma (c,d){
+    let res = c+d;
+    console.log(c+d)
+    return
+}
+
+soma(3,4)
+
+//4
+function soma (e,f,g,h){
+    console.log(e+f+g+h)
+    return
+}
+
+soma(3,4)\
